8d8179559022594b221970f03918607f82383117
Reno DuBois <me@renodubois.com>
2025-05-10 01:37:15 -0500

    Add ESLint to project

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+import svelte from "eslint-plugin-svelte";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{svelte}"], plugins: { svelte } },
+  tseslint.configs.recommended,
+  ...svelte.configs.recommended
+]);
